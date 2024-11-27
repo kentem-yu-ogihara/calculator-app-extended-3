14b073d34533a851f147bf00a574e292316658aa
荻原 友太 <yu-ogihara@kentem.co.jp>
2024-11-27 15:48:08 +0900

    update: テーマ設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
-const App: React.FC = () => (
+const App: React.FC = () => {
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  return (
   <div className="App">
+    <div className="theme-switcher">
+      <button onClick={toggleTheme}>
+        {theme === 'light' ? 'ダークテーマに切り替え' : 'ライトテーマに切り替え'}
+      </button>
+    </div>
     <Calculator />
   </div>
-);
+)};
 
 export default App;
